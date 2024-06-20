0126763c93929cfad6b7636de62bc82030efb13a
Miguel Coelho <miguel.pinheiro.coelho@tecnico.ulisboa.pt>
2024-06-20 20:44:23 +0100

    Initial commit

---
+++ src/main/java/jabberdrake/homesteading/Homesteading.java
@@ -0,0 +1,20 @@
+package jabberdrake.homesteading;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Homesteading implements ModInitializer {
+	public static final String MOD_ID = "homesteading";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/jabberdrake/homesteading/HomesteadingClient.java
@@ -0,0 +1,11 @@
+package jabberdrake.homesteading;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HomesteadingClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        //empty
+    }
+}
